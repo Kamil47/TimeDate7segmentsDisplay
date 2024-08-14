f244f54b1e89b4f8820cc0a56146b836af47852f
Kamil Zachej <kamil.zachej@gmail.com>
2024-08-14 18:52:43 +0200

    in code loop added  WiFi state and display intensity

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,21 @@
   #include <Ticker.h>
   #include <WiFiMulti.h>
   WiFiMulti wifiMulti;                          // create WiFi object
-
+                          
+  int wistat = 0;                               //0 not connected, 3 connected, 4 failed, 5 lost, 6 disconnected 
+  int lightPin = 34;                            //light sensor pin
+  int lightValue = 0;                           //light sensor value
+  int dispIntensity = 0;                        //intensity of 7 segment Led display
+  const int senMin = 500;                       //minimum intensity
+  const int senMax = 4000;                      //maximum intensity
+  const int ledPin = 32;                        //LED indicator ERROR
+  const int tempPin = 5;                        //optional temperature sensor DS18B20
+  
   constexpr uint8_t latchPin = 16;              // change to reflect your latchPin
   constexpr auto STASSID = "ID";
   constexpr auto STAPSK = "Password";
 
-  #define TZ_Europe_Bratislava  PSTR("CET-1CEST,M3.5.0,M10.5.0/3")
+  #define TZ_Europe_Bratislava  PSTR("CET-1CEST,M3.5.0,M10.5.0/3")    //change to your timezone
 
   constexpr uint8_t numDevices = 1; // the number of devices
   constexpr uint8_t numDigits = 6;
@@ -53,16 +62,14 @@ void setup() {
   static int currHour = 1;
    if (currHour != tm->tm_hour) {
       currHour = tm->tm_hour;
-      int32_t hour = currHour; ((tm->tm_hour % 12) != 0) ? tm->tm_hour % 12 : 12;
-      //hourDisp.writeNumber(hour, MAX72XX::Character::BLANK, 0);      
+      int32_t hour = currHour; ((tm->tm_hour % 12) != 0) ? tm->tm_hour % 12 : 12;        
     }
 
   // minute
    static int currMin = -1;
     if (currMin != tm->tm_min) {
       currMin = tm->tm_min;
-      //minDisp.writeNumber(tm->tm_min, MAX72XX::Character::ZERO, 0);
-    }
+      }
       int32_t hour = currHour; 
      
   // date, update if the day or year changes
@@ -72,9 +79,7 @@ void setup() {
     if (currDay != tm->tm_mday || currYear != tm->tm_year) {
       currDay = tm->tm_mday;
       currYear = tm->tm_year;
-      //dayDisp.writeNumber(tm->tm_mday, MAX72XX::Character::ZERO, 0);
-      //monDisp.writeNumber(tm->tm_mon + 1, MAX72XX::Character::ZERO, 0);
-      //yearDisp.writeNumber(tm->tm_year-100, MAX72XX::Character::BLANK);
+      
     }
     if (tm->tm_sec >52 & tm->tm_sec <59)  {
            
@@ -90,5 +95,19 @@ void setup() {
 }
 
   void loop() {
+    
+  wistat = WiFi.status();
+    if (wistat != 3)  {
+    digitalWrite(ledPin, HIGH);
+    }
+    else {
+    digitalWrite(ledPin, LOW);
+    } 
+
+    lightValue = analogRead(lightPin);
+    dispIntensity = (map(lightValue, senMin, senMax, 5, 0) - 1);
+    dispChain.setIntensity(dispIntensity);
+
   wifiMulti.run();            // conenct to wifi with reconnect if required
+  delay(100);                 // prevent unexpected display state during start
 }
